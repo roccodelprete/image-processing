d344c8523a5eb10b8e8dc675ce85010277ca8c5b
Rocco Del Prete <122788299+roccodelprete@users.noreply.github.com>
2024-07-09 14:29:11 +0200

    fix hough circles test

--- tests/hough-circles/main.cpp
+++ tests/hough-circles/main.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 using namespace cv;
 
-Mat drawCircles(Mat src, Mat edges, Mat votes, int minRadius, int maxRadius, int votesTh) {
+Mat drawCircles(Mat src, Mat edges, Mat votes, int votesTh, int minRadius, int maxRadius) {
 	Mat out = src.clone();
 
 	for (int radius = minRadius; radius <= maxRadius; radius++) {
-		for (int i = 0; i < edges.cols; i++) {
-			for (int j = 0; j < edges.rows; j++) {
-				if (votes.at<uchar>(j, i, radius - minRadius) > votesTh) {
-					circle(out, Point(i, j), 3, Scalar(0), 2);
-					circle(out, Point(i, j), radius, Scalar(0), 2);
+		for (int b = 0; b < edges.rows; b++) {
+			for (int a = 0; a < edges.cols; a++) {
+				if (votes.at<uchar>(b, a, radius - minRadius) > votesTh) {
+					circle(out, Point(a, b), 2, Scalar(0), 2);
+					circle(out, Point(a, b), radius, Scalar(0), 2);
 				}
 			}
 		}
@@ -21,9 +21,9 @@ Mat drawCircles(Mat src, Mat edges, Mat votes, int minRadius, int maxRadius, int
 	return out;
 }
 
-Mat houghCircles(Mat src, int lowTh, int highTh, int votesTh, int minRadius, int maxRadius) {
+Mat houghCircles(Mat src, int minRadius, int maxRadius, int lowTh, int highTh, int votesTh) {
 	int sizes[3] = { src.rows, src.cols, maxRadius - minRadius + 1 };
-	Mat votes = Mat::zeros(3, sizes, CV_8U), gauss, edges;
+	Mat gauss, edges, votes = Mat::zeros(3, sizes, CV_8U);
 	GaussianBlur(src, gauss, Size(5, 5), 0);
 	Canny(gauss, edges, lowTh, highTh);
 
@@ -32,10 +32,10 @@ Mat houghCircles(Mat src, int lowTh, int highTh, int votesTh, int minRadius, int
 			if (edges.at<uchar>(i, j) == 255) {
 				for (int radius = minRadius; radius <= maxRadius; radius++) {
 					for (int theta = 0; theta < 360; theta++) {
-						int x = j - radius * sin(theta * CV_PI / 180), y = i - radius * cos(theta * CV_PI / 180);
+						int a = j - radius * cos(theta * CV_PI / 180), b = i - radius * sin(theta * CV_PI / 180);
 
-						if (x >= 0 && y >= 0 && x < edges.cols && y < edges.rows) {
-							votes.at<uchar>(y, x, radius - minRadius)++;
+						if (a >= 0 && b >= 0 && a < edges.cols && b < edges.rows) {
+							votes.at<uchar>(b, a, radius - minRadius)++;
 						}
 					}
 				}
@@ -43,7 +43,7 @@ Mat houghCircles(Mat src, int lowTh, int highTh, int votesTh, int minRadius, int
 		}
 	}
 
-	return drawCircles(src, edges, votes, minRadius, maxRadius, votesTh);
+	return drawCircles(src, edges, votes, votesTh, minRadius, maxRadius);
 }
 
 int main() {
@@ -55,8 +55,8 @@ int main() {
 	}
 
 	imshow("original", img);
-	imshow("hough circles image", houghCircles(img, 160, 230, 140, 40, 90));
-	waitKey(0);
+	imshow("hough circles image", houghCircles(img, 40, 90, 160, 230, 140));
+	waitKey();
 
 	return 0;
 }
